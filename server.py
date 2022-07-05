a433b28b5ae75c6c536142de4c50edff7c5b131c
camarillobryanb <bryan_daniel_camarillo@dlsu.edu.ph>
2022-07-05 12:44:59 +0800

    server updates

--- server.py
+++ server.py
@@ -1,15 +1,22 @@
-from flask import Flask
+from flask import Flask, request, send_file
+import matplotlib.pyplot as plt
+from colorizers import *
 
 urls = ("/favicon.ico", "dummy")
-
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/api', methods=['GET'])
-def server():
-    return {
-        "tutorial": "Flask React Heroku "
-    }
+@app.route('/', methods = ['POST'])
+def getImage():
+    file = request.files['file']
+    
+    colorizer_siggraph17 = siggraph17(pretrained=True).eval()
+    (tens_l_orig, tens_l_rs) = preprocess_img(file, HW=(256,256))
+    out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
+
+    response = {"Hello":"Success"}
+    return response
+    #return send_file(out_img_siggraph17)
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
