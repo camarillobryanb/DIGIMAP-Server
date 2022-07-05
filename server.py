de0d0ca9f77d15c9259af16b4cd001b68ba567d1
camarillobryanb <bryan_daniel_camarillo@dlsu.edu.ph>
2022-07-05 13:31:57 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, send_file, render_template
-import matplotlib.pyplot as plt
-from colorizers import *
+#import matplotlib.pyplot as plt
+#from colorizers import *
 
 urls = ("/favicon.ico", "dummy")
 app = Flask(__name__)
@@ -9,16 +9,16 @@ app = Flask(__name__)
 def hello():
     return "hello"
 
-@app.route('/post', methods = ['POST'])
-def getImage():
-    file = request.files['file']
+#@app.route('/post', methods = ['POST'])
+#def getImage():
+    #file = request.files['file']
     
-    colorizer_siggraph17 = siggraph17(pretrained=True).eval()
-    (tens_l_orig, tens_l_rs) = preprocess_img(file, HW=(256,256))
-    out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
+    #colorizer_siggraph17 = siggraph17(pretrained=True).eval()
+    #(tens_l_orig, tens_l_rs) = preprocess_img(file, HW=(256,256))
+    #out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
 
-    response = {"Hello":"Success"}
-    return response
+    #response = {"Hello":"Success"}
+    #return response
     #return send_file(out_img_siggraph17)
 
 if __name__ == '__main__':
